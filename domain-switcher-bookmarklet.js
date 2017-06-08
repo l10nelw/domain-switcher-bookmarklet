9568b223104086b4819a5ce323736775f5cc529e
Lionel Wong <lionelster@gmail.com>
2017-06-08 11:36:45 +0800

    Include protocol to be manually specifiable

--- domain-switcher-bookmarklet.js
+++ domain-switcher-bookmarklet.js
@@ -1,33 +1,33 @@
 javascript:(()=>{
-  var list = [
-    ['localhost:8000','www.easyuni.com', true],
-    ['www.google.com.my/search?q=', 'duckduckgo.com/?q=', false],
-    ['open.spotify.com', 'play.spotify.com', false],
-    ['dontstarve.wikia.com/wiki', 'dontstarve.gamepedia.com', false],
-    ['www.dotabuff.com/matches','www.opendota.com/matches',false],
-  ];
-  var url = location.toString().split('//')[1];
-  var heads = [];
-  for (i in list) {
-    heads = heads.concat(list[i].slice(0,2));
-  }
-  for (i in heads) {
-    var A = heads[i];
-    if (url.startsWith(A)) {
-      var odd = i%2;
-      var col0idx = i-odd;
-      var B = heads[col0idx+!odd];
-      var newwin = list[col0idx/2][2];
-      break;
+    var list = [
+        ['http://localhost:8000','https://www.easyuni.com', true],
+        ['https://www.google.com.my/search?q=', 'https://duckduckgo.com/?q=', false],
+        ['https://open.spotify.com', 'https://play.spotify.com', false],
+        ['https://dontstarve.wikia.com/wiki', 'https://dontstarve.gamepedia.com', false],
+        ['https://www.dotabuff.com/matches','https://www.opendota.com/matches',false],
+    ];
+    var url = location.toString();
+    var heads = [];
+    for (i in list) {
+        heads = heads.concat(list[i].slice(0,2));
     }
-  }
-  if (B) {
-    url = '//'+url.replace(A, B);
-    if (newwin) {
-      void(window.open(url));
-    } else {
-      location.href = url;
+    for (i in heads) {
+        var A = heads[i];
+        if (url.startsWith(A)) {
+            var odd = i%2;
+            var col0idx = i-odd;
+            var B = heads[col0idx+!odd];
+            var newwin = list[col0idx/2][2];
+            break;
+        }
     }
-  }
+    if (B) {
+        url = url.replace(A, B);
+        if (newwin) {
+              void(window.open(url));
+        } else {
+          location.href = url;
+        }
+    }
 })()
 
