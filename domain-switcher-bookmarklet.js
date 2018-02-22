e79a893fa986b51cc0cc3218ba0b5200090c8f55
Lionel Wong <lionelster@gmail.com>
2018-02-22 13:29:28 +0800

    Rewrite to allow 3 domains in a set
    User can select between the 2 destinations

--- domain-switcher-bookmarklet.js
+++ domain-switcher-bookmarklet.js
@@ -1,33 +1,34 @@
-javascript:(()=>{
-    var list = [
-        ['http://localhost:8000','https://www.easyuni.com', true],
-        ['https://www.google.com.my/search?q=', 'https://duckduckgo.com/?q=', false],
-        ['https://open.spotify.com', 'https://play.spotify.com', false],
-        ['https://en.wikipedia.org/wiki', 'https://www.wikiwand.com/en', false],
-        ['https://www.dotabuff.com/matches','https://www.opendota.com/matches',false],
+javascript:(_=>{
+    var sets = [
+        [['http://localhost:8000', 'http://staging.easyuni.com', 'https://www.easyuni.com'], true],
+        [['https://www.google.com.my/search?q=', 'https://duckduckgo.com/?q='], false],
+        [['https://open.spotify.com', 'https://play.spotify.com'], false],
+        [['https://en.wikipedia.org/wiki', 'https://www.wikiwand.com/en'], false],
+        [['https://www.dotabuff.com/matches', 'https://www.opendota.com/matches'], false],
     ];
     var url = location.toString();
-    var heads = [];
-    for (i in list) {
-        heads = heads.concat(list[i].slice(0,2));
-    }
-    for (i in heads) {
-        var A = heads[i];
-        if (url.startsWith(A)) {
-            var odd = i%2;
-            var col0idx = i-odd;
-            var B = heads[col0idx+!odd];
-            var newwin = list[col0idx/2][2];
-            break;
+    var newwin = false, match = '', dest = [];
+    (_=>{
+        for (var set of sets) {
+            var heads = set[0];
+            newwin = set[1];
+            for (var head of heads) {
+                if (url.startsWith(head)) {
+                    match = head;
+                    dest = heads.filter(x => x !== match);
+                    return;
+                }
+            }
         }
+        throw 'Site not in array set';
+    })();
+    if (!dest.length) {
+        throw 'Error: No destination url produced';
+    } else if (dest.length > 1) {
+        dest = confirm(`Cancel = ${dest[1]}\nOK = ${dest[0]}`) ? dest[0] : dest[1];
+    } else {
+        dest = dest[0];
     }
-    if (B) {
-        url = url.replace(A, B);
-        if (newwin) {
-              void(window.open(url));
-        } else {
-          location.href = url;
-        }
-    }
+    url = url.replace(match, dest);
+    newwin ? void(open(url)) : location.href = url;
 })()
-
