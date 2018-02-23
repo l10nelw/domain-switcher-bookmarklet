9171dae51c0de7215cc46a02a716355d7bfc16b8
Lionel Wong <lionelster@gmail.com>
2018-02-23 12:22:35 +0800

    Prettify. Clarify vars with const/let

--- domain-switcher-bookmarklet.js
+++ domain-switcher-bookmarklet.js
@@ -1,17 +1,22 @@
 javascript:(_=>{
-    var sets = [
+    
+    const SETS = [
         [['http://localhost:8000', 'http://staging.easyuni.com', 'https://www.easyuni.com'], true],
         [['https://www.google.com/search?q=', 'https://duckduckgo.com/?q='], false],
         [['https://en.wikipedia.org/wiki', 'https://www.wikiwand.com/en'], false],
         [['https://www.dotabuff.com/matches', 'https://www.opendota.com/matches'], false],
     ];
-    var url = location.toString();
-    var newwin = false, match = '', dest = [];
+    
+    var url = location.toString(),
+        newwin = false,
+        match = '',
+        dest = [];
+    
     (_=>{
-        for (var set of sets) {
-            var heads = set[0];
+        for (let set of SETS) {
+            let heads = set[0];
             newwin = set[1];
-            for (var head of heads) {
+            for (let head of heads) {
                 if (url.startsWith(head)) {
                     match = head;
                     dest = heads.filter(x => x !== match);
@@ -21,6 +26,7 @@ javascript:(_=>{
         }
         throw 'Site not in array set';
     })();
+    
     if (!dest.length) {
         throw 'Error: No destination url produced';
     } else if (dest.length > 1) {
@@ -28,6 +34,8 @@ javascript:(_=>{
     } else {
         dest = dest[0];
     }
+    
     url = url.replace(match, dest);
     newwin ? void(open(url)) : location.href = url;
+    
 })()
